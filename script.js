3854fab6c70ce35b46a1466f193e264e99c2657a
abdulkadirCildir <a.cildir89@gmail.com>
2021-02-01 15:52:50 +0100

    update date and time

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const daysEl = document.getElementById("days");
 const hoursEl = document.getElementById("hours");
 const minutesEl = document.getElementById("minutes");
 const secondsEl = document.getElementById("seconds");
+const today = document.getElementById("today");
+const clock = document.getElementById("clock");
 
 
-const newYears = "1 Jan 2021";
+const newYears = "1 Jan 2022";
 
 function countdown() {
     const newYearsDate = new Date(newYears);
@@ -12,8 +14,8 @@ function countdown() {
 
     const totalSeconds = (newYearsDate - currentDate) / 1000;
 
-    const days = Math.floor(totalSeconds / 3600 / 24);
-    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const days = Math.floor(totalSeconds / 60 / 60 / 24);
+    const hours = Math.floor(totalSeconds / 60 / 60) % 24;
     const minutes = Math.floor(totalSeconds / 60) % 60;
     const seconds = Math.floor(totalSeconds) % 60;
 
@@ -21,6 +23,18 @@ function countdown() {
     hoursEl.innerHTML = hours;
     minutesEl.innerHTML = minutes;
     secondsEl.innerHTML = seconds;
+
+
+    let weekday = ['Sunday',
+               'Monday',
+               'Tuesday',
+               'Wednesday',
+               'Thursday',
+               'Friday',
+               'Saturday'][new Date().getDay()];
+    today.innerHTML = String(currentDate.getDate()).padStart(2, "0") + " . " + String(currentDate.getMonth() + 1).padStart(2, "0") + " . " + currentDate.getFullYear() + " " + weekday
+
+    clock.innerHTML = new Date().toLocaleTimeString();
 
     console.log(days, hours, minutes, seconds);
 }
